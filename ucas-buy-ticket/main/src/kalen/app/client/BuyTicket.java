cb74e2fe441349852e08cdc20ce6236f847bbdd8
nladuo <kalen25115@gmail.com>
2018-05-10 15:19:27 +0800

    updated directory structure.

---
+++ ucas-buy-ticket/main/src/kalen/app/client/BuyTicket.java
@@ -0,0 +1,18 @@
+package kalen.app.client;
+//import net.dongliu;
+
+/**
+ * Created by kalen on 2018/5/10.
+ */
+public class BuyTicket {
+
+
+    public static void main(String args[]) {
+
+
+
+
+
+
+    }
+}
